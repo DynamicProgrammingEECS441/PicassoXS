4bd2d12f37bd27bec87098ad0c86932a8f191edb
vagrant <vagrant@vagrant.vm>
2020-03-03 19:57:15 +0000

    Add main.py and command line argument parser.

--- src/backend/train.py
+++ src/backend/train.py
@@ -1,4 +1,23 @@
 import tensorflow as tf
 from tensorflow.keras.layers import Dense, Flatten, Conv2D
 import numpy
-import cv2
+
+import os
+import time
+from glob import glob
+import tensorflow as tf
+import numpy as np
+from collections import namedtuple
+from tqdm import tqdm
+import multiprocessing
+
+import imageio
+from PIL import Image
+
+
+class Model(object):
+    def __init__(self, sess, args):
+        self.sess = sess
+    
+    def train(self, args, ckpt_nmbr=None):
+        pass
