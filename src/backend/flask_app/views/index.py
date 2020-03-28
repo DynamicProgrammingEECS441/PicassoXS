00672e79b56742b280adc4f16680c28e0a7bc093
Enhao Zhang <ehzhang@umich.edu>
2020-03-28 17:15:57 -0400

    flask app

--- src/backend/flask_app/views/index.py
+++ src/backend/flask_app/views/index.py
@@ -11,4 +11,10 @@ import flask_app
 @flask_app.app.route('/upload_img/', methods=['POST'])
 def upload_img():
     content_img = flask.request.files['file']
+    print(content_img)
+    context = {
+        "status": "success",
+    }
+    return flask.jsonify(**context)
+
     # return flask.render_template("index.html", **context)
