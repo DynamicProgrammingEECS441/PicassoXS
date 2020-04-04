6003afad11c92837af5230536099e381c3f17e0f
Enhao Zhang <ehzhang@umich.edu>
2020-04-04 12:03:35 -0400

    changed default iamge size

--- src/backend/general_model/train.sh
+++ src/backend/general_model/train.sh
@@ -1,5 +1,6 @@
 CUDA_VISIBLE_DEVICES="" python main.py \
                  --model_name=model_van-gogh_new \
                  --phase=train \
-                 --ptcd=/path/to/Places2/data_large \
-                 --ptad=./data/vincent-van-gogh_road-with-cypresses-1890
+                 --image_size=762 \
+                 --ptcd=../data/sample_photographs/ \
+                 --ptad=../data/vincent-van-gogh_road-with-cypresses-1890
