db9875242d9ade3b58028f7b8010b0968f9e5dc9
Enhao Zhang <ehzhang@umich.edu>
2020-04-05 19:00:21 -0400

    clean up

--- src/frontend/PicassoXS/DerivedData/PicassoXS/Build/Intermediates.noindex/Pods.build/Debug-iphonesimulator/AlamofireImage.build/Objects-normal/x86_64/AlamofireImage-dummy.d
+++
@@ -1,4 +0,0 @@
-dependencies: \
-  /Users/zhangenhao/Desktop/UM/2020_Winter/EECS_441/PicassoXS/src/frontend/PicassoXS/DerivedData/PicassoXS/Build/Products/Debug-iphonesimulator/Alamofire/Alamofire.modulemap \
-  /Users/zhangenhao/Desktop/UM/2020_Winter/EECS_441/PicassoXS/src/frontend/PicassoXS/Pods/Target\ Support\ Files/AlamofireImage/AlamofireImage-dummy.m \
-  /Users/zhangenhao/Desktop/UM/2020_Winter/EECS_441/PicassoXS/src/frontend/PicassoXS/Pods/Target\ Support\ Files/AlamofireImage/AlamofireImage-prefix.pch
