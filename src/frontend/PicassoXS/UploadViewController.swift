985cc4139a41b63a7a253cca991f021296b95026
Enhao Zhang <ehzhang@umich.edu>
2020-04-05 19:01:37 -0400

    send request for arbitrary style model

--- src/frontend/PicassoXS/UploadViewController.swift
+++ src/frontend/PicassoXS/UploadViewController.swift
@@ -29,7 +29,8 @@ class UploadViewController: UIViewController{
             super.viewDidLoad()
             InputIm.image = GlobalVariables.curInput
             FilterIm.image = GlobalVariables.curFilter
-            sendRequest(Image: InputIm.image!)
+//            sendRequest(Image: InputIm.image!)
+            sendRequestWithStyle(Image: InputIm.image!, ImageStyle: FilterIm.image!)
         }
     
     //Upload Filter Image for Filtering
@@ -38,7 +39,8 @@ class UploadViewController: UIViewController{
         let imgData = Image.jpegData(compressionQuality: 1)!
         AF.upload(multipartFormData: { multipartFormData in
             multipartFormData.append(imgData, withName: "document", fileName: "document.png", mimeType: "image/png")
-        }, to: "http://localhost:8000/upload_img/")
+        }, to: "http://localhost:8000/general_model_grpc/")
+//        }, to: "https://tensorflow-serving-9905.appspot.com/upload_img/")
             .responseImage { response in
                 if case .success(let image) = response.result {
                     print("image downloaded: \(image)")
@@ -46,6 +48,24 @@ class UploadViewController: UIViewController{
                 }
             }
     }
+    
+    func sendRequestWithStyle(Image: UIImage, ImageStyle: UIImage){
+        let imgData = Image.jpegData(compressionQuality: 1)!
+        let imgStyleData = ImageStyle.jpegData(compressionQuality: 1)!
+        
+        AF.upload(multipartFormData: { multipartFormData in
+            multipartFormData.append(imgData, withName: "content_img", fileName: "content.png", mimeType: "image/png")
+            multipartFormData.append(imgStyleData, withName: "style_img", fileName: "style.png", mimeType: "image/png")
+        }, to: "http://localhost:8000/arbitrary_style_grpc/")
+//        }, to: "https://tensorflow-serving-9905.appspot.com/upload_img/")
+            .responseImage { response in
+                if case .success(let image) = response.result {
+                    print("image downloaded: \(image)")
+                    self.OutputIm.image = image
+                }
+            }
+    }
+    
 }
     
 
