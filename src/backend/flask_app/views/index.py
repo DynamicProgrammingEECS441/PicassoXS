acc51cb01912d32d0282b440aea43cc8cd912ad3
Enhao Zhang <ehzhang@umich.edu>
2020-04-05 17:39:05 -0400

    added gRPC method

--- src/backend/flask_app/views/index.py
+++ src/backend/flask_app/views/index.py
@@ -12,6 +12,40 @@ import json
 import requests
 from PIL import Image
 import io
+import grpc
+import tensorflow as tf
+from tensorflow_serving.apis import predict_pb2 # TODO you need to download tensorflow_serving on your server 
+from tensorflow_serving.apis import prediction_service_pb2_grpc
+
+def img_resize(img):
+    '''
+    Input:
+        img (PIL.Image) : <PIL.Image> class objecy represent image 
+    '''
+    h = img.shape[0]
+    w = img.shape[1]
+    IMG_LONG_SIZE = 700.
+
+    if h > w : # h is the long side 
+        h_new = int(IMG_LONG_SIZE)
+        w_new = int(IMG_LONG_SIZE * (w * (1.0) ) / ( h  * (1.0) ) )
+    else:      # w is the long side 
+        w_new = int(IMG_LONG_SIZE)
+        h_new = int(IMG_LONG_SIZE * (h * (1.0) ) / ( w  * (1.0) ) )
+    img = cv2.resize(img, (w_new, h_new), interpolation=cv2.INTER_CUBIC)
+    return img 
+    
+def post_process(img):
+    '''
+    Input:
+        img (np.array) :  value range : [-1, 1], dtype : float32
+    Return:
+        img (np.array) :  value range : [0, 255], dtype : uint8 
+        # TODO change the return image having the same image range as the image you received 
+    '''
+    img = (img + 1.) * 127.5
+    img = img.astype(np.uint8)
+    return img 
 
 @flask_app.app.route('/upload_img/', methods=['POST'])
 def upload_img():
@@ -55,6 +89,45 @@ def upload_img():
 
     # convert numpy array to PIL Image
     output_img = Image.fromarray(output_img)
+
+    # create file-object in memory
+    file_object = io.BytesIO()
+
+    # write PNG in file-object
+    output_img.save(file_object, 'PNG')
+
+    # move to beginning of file so `send_file()` it will read from start    
+    file_object.seek(0)
+
+    return flask.send_file(file_object, mimetype='image/PNG')
+
+@flask_app.app.route('/general_model_grpc/', methods=['POST'])
+def general_model_grpc():
+    filestr = flask.request.files.to_dict()['document'].read()
+    npimg = np.fromstring(filestr, np.uint8)
+    img = cv2.imdecode(npimg, cv2.IMREAD_UNCHANGED)
+
+    img = img_resize(img)
+    img = np.expand_dims(np.array(img).astype(np.float32), axis=0)  # float32, (1, h, w, 3) representaiton 
+
+    # 3. Prepare & Send Request 
+    ip_port = "0.0.0.0:32770"  # TODO change this to your ip:port 
+    channel = grpc.insecure_channel(ip_port)
+    stub = prediction_service_pb2_grpc.PredictionServiceStub(channel)
+    request = predict_pb2.PredictRequest()
+    request.model_spec.name = "monet"  # TODO change this to the model you're using 
+    request.model_spec.signature_name = "predict_images" 
+    request.inputs["input_img"].CopyFrom(  
+        tf.make_tensor_proto(img, shape=list(img.shape))) 
+    response = stub.Predict(request, 10.0)  # TODO change the request timeout, default is 10s
+
+    # 4. Image Postprocess 
+    output_img = tf.make_ndarray(response.outputs['output_img'])  # numpy array (1, H, W, 3)
+    output_img = post_process(output_img)
+
+    # 5. Save Image / Send image back to frontend 
+    output_img = Image.fromarray(output_img[0])
+    output_img.save('test_gRPC_img1.jpg')
 
     # create file-object in memory
     file_object = io.BytesIO()
