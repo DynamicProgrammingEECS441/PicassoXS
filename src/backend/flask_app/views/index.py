7ea7f55fda2d8400c417124877c1dcf06afda928
Enhao Zhang <ehzhang@umich.edu>
2020-04-09 21:49:48 -0400

    added Self-made retry mechanism with exp. backoff

--- src/backend/flask_app/views/index.py
+++ src/backend/flask_app/views/index.py
@@ -16,6 +16,75 @@ import grpc
 import tensorflow as tf
 from tensorflow_serving.apis import predict_pb2 # TODO you need to download tensorflow_serving on your server 
 from tensorflow_serving.apis import prediction_service_pb2_grpc
+from random import randint
+import time
+from typing import Optional, Tuple
+
+class SleepingPolicy():
+    def sleep(self, try_i: int):
+        """
+        How long to sleep in milliseconds.
+        :param try_i: the number of retry (starting from zero)
+        """
+        assert try_i >= 0
+
+class ExponentialBackoff(SleepingPolicy):
+    def __init__(self, *, init_backoff_ms: int, max_backoff_ms: int, multiplier: int):
+        self.init_backoff = randint(0, init_backoff_ms)
+        self.max_backoff = max_backoff_ms
+        self.multiplier = multiplier
+
+    def sleep(self, try_i: int):
+        sleep_range = min(
+            self.init_backoff * self.multiplier ** try_i, self.max_backoff
+        )
+        sleep_ms = randint(0, sleep_range)
+        print("sleeping for ", sleep_ms)
+        time.sleep(sleep_ms / 1000)
+
+class RetryOnRpcErrorClientInterceptor(
+    grpc.UnaryUnaryClientInterceptor, grpc.StreamUnaryClientInterceptor
+):
+    def __init__(
+        self,
+        *,
+        max_attempts: int,
+        sleeping_policy: SleepingPolicy,
+        status_for_retry: Optional[Tuple[grpc.StatusCode]] = None,
+    ):
+        self.max_attempts = max_attempts
+        self.sleeping_policy = sleeping_policy
+        self.status_for_retry = status_for_retry
+
+    def _intercept_call(self, continuation, client_call_details, request_or_iterator):
+
+        for try_i in range(self.max_attempts):
+            response = continuation(client_call_details, request_or_iterator)
+
+            if isinstance(response, grpc.RpcError):
+
+                # Return if it was last attempt
+                if try_i == (self.max_attempts - 1):
+                    return response
+
+                # If status code is not in retryable status codes
+                if (
+                    self.status_for_retry
+                    and response.code() not in self.status_for_retry
+                ):
+                    return response
+
+                self.sleeping_policy.sleep(try_i)
+            else:
+                return response
+
+    def intercept_unary_unary(self, continuation, client_call_details, request):
+        return self._intercept_call(continuation, client_call_details, request)
+
+    def intercept_stream_unary(
+        self, continuation, client_call_details, request_iterator
+    ):
+        return self._intercept_call(continuation, client_call_details, request_iterator)
 
 def img_resize(img):
     '''
@@ -62,10 +131,19 @@ def general_model_grpc():
 
     # 3. Prepare & Send Request 
     # ip_port = "0.0.0.0:32770"  # TODO change this to your ip:port 
+
+    interceptors = (
+        RetryOnRpcErrorClientInterceptor(
+            max_attempts=4,
+            sleeping_policy=ExponentialBackoff(init_backoff_ms=100, max_backoff_ms=1600, multiplier=2),
+            status_for_retry=(grpc.StatusCode.UNAVAILABLE,),
+        ),
+    )
+
     ip_port = "35.232.203.191:8500"
     channel_opt = [('grpc.max_send_message_length', 512 * 1024 * 1024), ('grpc.max_receive_message_length', 512 * 1024 * 1024)]
     channel = grpc.insecure_channel(ip_port, options=channel_opt)
-    stub = prediction_service_pb2_grpc.PredictionServiceStub(channel)
+    stub = prediction_service_pb2_grpc.PredictionServiceStub(grpc.intercept_channel(channel, *interceptors))
     request = predict_pb2.PredictRequest()
     request.model_spec.name = model_name  # TODO change this to the model you're using 
     request.model_spec.signature_name = "predict_images" 
@@ -116,15 +194,17 @@ def arbitrary_style_grpc():
     style_img_np = np.expand_dims(style_img_np, axis=0) # float32, (1, h, w, 3) representaiton 
 
     # 3. Prepare & Send Request 
-    # ip_port = "0.0.0.0:32768"  # TODO change this to your ip:port 
+    interceptors = (
+        RetryOnRpcErrorClientInterceptor(
+            max_attempts=4,
+            sleeping_policy=ExponentialBackoff(init_backoff_ms=100, max_backoff_ms=1600, multiplier=2),
+            status_for_retry=(grpc.StatusCode.UNAVAILABLE,),
+        ),
+    )
     ip_port = "35.232.203.191:8500"
     channel_opt = [('grpc.max_send_message_length', 512 * 1024 * 1024), ('grpc.max_receive_message_length', 512 * 1024 * 1024)]
     channel = grpc.insecure_channel(ip_port, options=channel_opt)
-    # if you run docker run -t -p 0000:8500 -p 0001:8501 xiaosong99/servable:latest-skeleton 
-    # then the port should be "0000"
-    # For more information, see `QuickStart_GeneralModel.md`
-    # channel = grpc.insecure_channel(ip_port)
-    stub = prediction_service_pb2_grpc.PredictionServiceStub(channel)
+    stub = prediction_service_pb2_grpc.PredictionServiceStub(grpc.intercept_channel(channel, *interceptors))
     request = predict_pb2.PredictRequest()
     request.model_spec.name = "arbitary_style" # TODO change this to the model you're using 
     request.model_spec.signature_name = "predict_images" 
